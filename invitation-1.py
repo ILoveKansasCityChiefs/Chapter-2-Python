2798219f1b79fa41e8ce43a313067bcb5a0843bb
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-03 10:50:29 -0400

    Add invitations for three people to dinner

---
+++ invitation-1.py
@@ -0,0 +1,13 @@
+people_invited = ['Jesus', 'Mom', 'Tito']
+
+# first invitation
+
+messageA = f"Hey {people_invited[0].title()}, you are invited to my dinner!"
+print(messageA)
+
+# second invitation
+messageB = f"Hey {people_invited[1].title()}, you are invited to my dinner!"
+print(messageB)
+# third message
+messageC = f"Hey {people_invited[2].title()}, you are invited to my dinner!"
+print(messageC)
