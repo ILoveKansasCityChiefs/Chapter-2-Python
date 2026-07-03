c841f83cd137655aec33455db56d88170caf4a7f
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-03 10:33:16 -0400

    Add date comment to friends-2.py

---
+++ friends-2.py
@@ -0,0 +1,16 @@
+# JUly 3, 2026
+friends = ['Adryan', 'Malakiyah', 'Darbyn', 'Meekdad', 'Michael']
+messageA = f"Hello {friends[0].title()}!"
+print(messageA)
+
+messageB = f"Hello {friends[1].title()}!"
+print(messageB)
+
+messageC = f"Hello {friends[2].title()}!"
+print(messageC)
+
+messageD = f"Hello {friends[3].title()}!"
+print(messageD)
+
+messageE = f"Hello {friends[4].title()}!"
+print(messageE)
